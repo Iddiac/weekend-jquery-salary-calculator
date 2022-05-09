00a53b52562b36290259c8297618f9e5872a4f9e
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-05-08 22:36:57 -0500

    completed

--- script.js
+++ script.js
@@ -10,15 +10,26 @@ function onReady() {
     } 
     */
     $(`#Submit`).on('click', submit);
+    $(`#Submit`).on('click', monthlyCalc);
+    $(`.body`).on(`click`, '.delete-button', remove);
 };
+let monthly = 0;
+let annual = [];
 // THIS FUNCTION IS AMAZING AND WORKSSS
 function submit() {
     console.log('im in submit');
     let First = $(`#First-Name`).val();
+    $("#First-Name").val('');
     let Last = $(`#Last-Name`).val();
+    $("#Last-Name").val('');
     let ID = $(`#Id-Number`).val();
+    $("#Id-Number").val('');
     let Title = $(`#Title`).val();
-    let AnnualSalary= $(`#Annual-Salary`).val();
+    $("#Title").val('');
+    let AnnualSalary = $(`#Annual-Salary`).val();
+    $("#Annual-Salary").val('');
+    annual.push(AnnualSalary);
+
 
     $(`.body`).prepend(`
         <tr>
@@ -26,11 +37,28 @@ function submit() {
             <td> ${Last}</td>
             <td> ${ID}</td>
             <td> ${Title}</td>
-            <td> ${AnnualSalary}</td>
-
-
+            <td> $${AnnualSalary}</td>
+            <td> <button class="delete-button"> delete </button> </td>
         </tr>
         
     `);
 
+}
+function remove(event) {
+    $(event.target).closest('tr').remove();
+
+}
+
+
+function monthlyCalc() {
+    for (let i = 0; i < annual.length; i++) {
+        monthly += annual[i];
+    }
+    monthly /= 12;
+    $('#Total').empty();
+    $("#Total").append('Monthly Cost ', monthly);
+    if (monthly > 20000) {
+        $(".below").addClass('Red');
+
+    }
 }
